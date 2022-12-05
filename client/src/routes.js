bdcd014eaa0cac7cc3c6db07c61eba043519d53c
Mark Moloney <markmo@me.com>
2022-12-05 19:55:10 +1100

    remove redundant client routes

--- client/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-const Example = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route element={<Home />} path="/" />
-    </Routes>
-  </BrowserRouter>
-);
